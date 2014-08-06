3f2a2e590d568c708d00d555e031c580e66ff1f0
Erik Timmers <e.timmers@gmail.com>
2014-08-06 08:48:49 +0200

    Restore Wheezy compatibility & fixup

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-ykfde
-=====
+# ykfde
 YubiKey Full Disk Encryption
 
 (yubikey support for LUKS)
 
-
-Quick start:
-------------
+## Quick start:
 
 First install the package, then do something like the following (with your
 yubikey plugged in!)
@@ -17,40 +14,38 @@ On bootup, you will be asked to insert a yubikey (2.2 or newer) which
 will then provide the response.  If you do not want to use a yubikey,
 press enter and then enter a normal passphrase during bootup.
 
-Change the key
---------------
+## Change the key
 
     # ykfdectl update
 
-Changing the key on each reboot
--------------------------------
+## Changing the key on each reboot
 
 This is still very basic.
 
     # update-rc.d ykfde defaults
 
-Configuration
--------------
-see =/etc/default/ykfde=
+## Configuration
+
+See _/etc/default/ykfde_
 
 Normaly used to select only the slot
-    
+
     YKOPTS="-2"
 
 The device which is used
-    
+
     LUKS_DEVICE="/dev/sda2"
 
 LUKS slot, where your yubikey-challenged key is stored
-    
+
     LUKS_SLOT="7"
 
-the challenge file, which is used to get a response
-    
+The challenge file, which is used to get a response
+
     CHALLENGE_FILE="/boot/yubikey-challenge"
 
 
-Limitations/bugs:
------------------
-* might need more error-handling.
+## Limitations/bugs:
+
+* might need more error-handling
 * lacks a two-factor-mode
--- src/ykfde.sh
+++ src/ykfde.sh
@@ -1,102 +1,139 @@
-#!/bin/bash -e
+#!/bin/sh
 
 # set the defaults
 YKOPTS="-2"
 LUKS_DEVICE="/dev/sda2"
 LUKS_SLOT="7"
 CHALLENGE_FILE="/boot/yubikey-challenge"
+ERROR_LOG="/var/log/ykfde"
 
 
 # read defaults..
 if [ -e /etc/default/ykfde ]; then
     . /etc/default/ykfde
 
-    if [ "${LUKS_SLOT}" == "multi" ]; then
-        echo -ne "LUKS_SLOT is set to 'multi': "
+    if [ "${LUKS_SLOT}" = "multi" ]; then
+        type ykinfo > /dev/null;
+        if [ $? -ne 0 ]; then
+            log_error "Multiple Yubikey support requires yubikey-personalization >= 1.8."
+            echo "ykinfo: not found"
+            exit 1
+        fi
+        echo -n "LUKS_SLOT is set to 'multi': "
         . /etc/ykfde.conf
-        YK_SERIAL="$(ykinfo -s | awk '{print $NF}')"
-        for (( i = 0; i < 8; i++ )); do
-            if [ "${LUKS_SLOT[${i}]}" == "${YK_SERIAL}" ]; then
+        serial="$(ykinfo -s | awk '{print $NF}')"
+        for i in $(seq 0 7); do
+            if [ "${LUKS_SLOT[${i}]}" == "${serial}" ]; then
                 LUKS_SLOT=${i}
                 echo "Using slot ${i}"
             fi
         done
     fi
-    if [ "${LUKS_SLOT}" == "multi" ]; then
-        echo "No matching Yubikey found".
+    if [ "${LUKS_SLOT}" = "multi" ]; then
+        echo "No matching Yubikey found."
         exit 2
     fi
 fi
 
 
-function get_new_challenge () { # {{{
+log_error () {
+    echo "$1" >> ${ERROR_LOG}
+}
+
+chalresp () {
+    key="$(ykchalresp ${YKOPTS} $1)"
+
+    if [ $? -ne 0 ]; then
+        mv ${CHALLENGE_FILE}.old ${CHALLENGE_FILE} 2>/dev/null
+        rm -f ${CHALLENGE_FILE}.new
+        log_error "Challenge-response failed."
+        echo "Failed"
+        exit 1
+    fi
+
+    echo "${key}"
+}
+
+
+
+get_new_challenge () {
     if [ -z ${CHALLENGE} ]; then
         export CHALLENGE="$(base64 < /dev/urandom | head -c 64)"
-        if ykinfo $YKOPTS >/dev/null ; then
-            echo -ne "${CHALLENGE}" > ${CHALLENGE_FILE}.new
-            chmod 400 ${CHALLENGE_FILE}.new
-        else
-            exit 1
-        fi
+        echo -n "${CHALLENGE}" > ${CHALLENGE_FILE}.new
+        chmod 400 ${CHALLENGE_FILE}.new
     fi
-} # }}}
+}
 
-function get_old_challenge () { # {{{
+get_old_challenge () {
     if [ -z ${OLD_CHALLENGE} ]; then
         export OLD_CHALLENGE="$(cat ${CHALLENGE_FILE})"
         mv ${CHALLENGE_FILE} ${CHALLENGE_FILE}.old
     fi
-} # }}}
+}
 
-function add_new_key () { # {{{
-    cryptsetup luksAddKey \
-        --key-slot ${LUKS_SLOT} ${LUKS_DEVICE} \
-        <(ykchalresp ${YKOPTS} ${CHALLENGE})
+add_new_key () {
 
-    if [ "${?}" == "0" ]; then
-        mv  ${CHALLENGE_FILE}.new ${CHALLENGE_FILE}
-    fi
-} # }}}
+    key=$(chalresp ${CHALLENGE})
 
-function update_key () { # {{{
-    OLD_KEY="$(ykchalresp ${YKOPTS} ${OLD_CHALLENGE})"
+    response=$(cryptsetup luksAddKey \
+            --key-slot ${LUKS_SLOT} ${LUKS_DEVICE} ${key} 2>&1)
 
-    cryptsetup luksChangeKey \
-        --key-slot ${LUKS_SLOT} ${LUKS_DEVICE} \
-        --key-file <( echo "${OLD_KEY}" ) <(ykchalresp ${YKOPTS} ${CHALLENGE})
-
-    if [ "${?}" == "0" ]; then
-        mv  ${CHALLENGE_FILE}.new ${CHALLENGE_FILE} && \
-        rm  -f ${CHALLENGE_FILE}.old
+    if [ $? -ne 0 ]; then
+        log_error "Failed adding LUKS key to cryptsetup."
+        log_error "${response}"
+        echo "Failed"
+        exit 1
     fi
 
-} # }}}
+    mv ${CHALLENGE_FILE}.new ${CHALLENGE_FILE}
+}
 
-function print_help () { # {{{
+update_key () {
+
+    oldKey=$(chalresp ${OLD_CHALLENGE})
+    newKey=$(chalresp ${CHALLENGE})
+
+    response=$(cryptsetup luksChangeKey \
+            --key-slot ${LUKS_SLOT} ${LUKS_DEVICE} \
+            --key-file ${oldKey} ${newKey} 2>&1)
+
+    if [ $? -ne 0 ]; then
+        mv ${CHALLENGE_FILE}.old ${CHALLENGE_FILE}
+        log_error "Failed changing LUKS key in cryptsetup."
+        log_error "${response}"
+        echo "Failed"
+        exit 1
+    fi
+
+    mv ${CHALLENGE_FILE}.new ${CHALLENGE_FILE}
+    rm -f ${CHALLENGE_FILE}.old
+
+}
+
+print_help () {
 
     echo "Usage: $0 (new|update|start|stop)"
 
-} # }}}
+}
 
 case $1 in
     new)
-        echo -ne "getting new key..."
+        echo -n "Getting new key... "
         get_new_challenge && echo "OK"
-        echo -ne "adding new key..."
+        echo -n "Adding new key... "
         add_new_key && echo "OK"
         /usr/sbin/update-initramfs -u -k "$(uname -r)"
 
     ;;
     update|start)
-        echo -ne "getting keys..."
-        get_new_challenge && echo -ne '.'
-        get_old_challenge && echo 'OK'
-        echo -ne "changing key..."
+        echo -n "Getting keys... "
+        get_new_challenge && get_old_challenge && echo 'OK'
+        echo -n "Changing key... "
         update_key && echo 'OK'
         /usr/sbin/update-initramfs -u -k "$(uname -r)"
     ;;
     stop)
-        echo "Nothing to do"
+        echo "Finished."
     ;;
     *)
         print_help
